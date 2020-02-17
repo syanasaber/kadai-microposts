74b6b356649afb7d572b540e1ec41d31bca77022
tadashi.horii <sermentofsyana@gmail.com>
2020-02-17 12:51:10 +0000

    revise micropost

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,14 +5,14 @@ class LikesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = '投稿をお気に入りしました'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path) 
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
   
   
